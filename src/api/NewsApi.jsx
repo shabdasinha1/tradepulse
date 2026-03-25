d9d2bf85320664d7b2a16b6c03a0ecfa160b5c88
shabdasinha1 <shabdasinha11@gmail.com>
2026-03-25 16:55:16 +0530

    updated code

--- src/api/NewsApi.jsx
+++ src/api/NewsApi.jsx
@@ -1,13 +1,8 @@
 import axios from "axios";
 
-const NEWS_BASE_URL = import.meta.env.VITE_PROXY_API_BASE_URL;
-
 const NewsApi = axios.create({
-  baseURL: NEWS_BASE_URL, 
+  baseURL: "",   // ✅ VERY IMPORTANT (empty)
   timeout: 10000,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 export default NewsApi;
